9ae8d8e9b8f9e3d19ef5c77f7a0d385bbe3909b8
Roy Li <me@dada.li>
2021-01-17 16:26:07 +0800

    chore: add types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,34 @@
 declare module 'scrollparent' {
   export default function (element: Element): Element
 }
+
+declare module 'smoothscroll-polyfill' {
+  export function polyfill(): void
+}
+
+declare module 'rangy' {
+  interface RangyHighlighter {
+    addClassApplier(classApplier: RangyClassApplier): void
+    highlightSelection(className: string): void
+  }
+
+  interface RangyClassApplier {
+    applyToSelection(win: Window): void
+  }
+
+  interface RangyExtendedStatic {
+    init(): void
+    createHighlighter(): RangyHighlighter
+    createClassApplier(
+      className: string,
+      options?: {
+        ignoreWhiteSpace?: boolean
+        tagNames?: string[]
+      },
+    ): RangyClassApplier
+  }
+
+  const rangy: RangyStatic & RangyExtendedStatic
+
+  export = rangy
+}
